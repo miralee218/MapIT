b5fb1898b698044848cce0baca4fbb7d1ce91f10
Mira <applesharonme@gmail.com>
2019-04-28 19:38:10 +0800

    view photo in album page.

--- MapIT/Controller/NotificationViewController.swift
+++ MapIT/Controller/NotificationViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Photos
+import CoreData
 
 class NotificationViewController: UIViewController {
 
@@ -17,6 +18,11 @@ class NotificationViewController: UIViewController {
             collectionView.dataSource = self
         }
     }
+    @IBOutlet weak var noDataView: UIView!
+    var allTravel: [Travel]?
+    var travel: Travel?
+    var count = 0
+    var photos = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
     navigationController?.navigationBar.setGradientBackground(
@@ -25,26 +31,84 @@ class NotificationViewController: UIViewController {
         collectionView.mr_registerCellWithNib(
             identifier: String(describing: NormalPictureCollectionViewCell.self), bundle: nil)
     }
-//    func grabPhoto() {
-//        let imgManager = PHImageManager.default()
-//        let requestOptions = PHImageRequestOptions()
-//        requestOptions.isSynchronous = true
-//        requestOptions.deliveryMode = .highQualityFormat
-//        let fetchOptions = PHFetchOptions()
-//        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-//        if let fetchResult: PHFetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions){
-//            if fetchResult.count > 0 {
-//
-//                
-//            }
-//        }
-//    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getTravel()
+    }
+    func getTravel() {
+        let fetchRequest: NSFetchRequest<Travel> = Travel.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: #keyPath(Travel.createTimestamp), ascending: true)]
+        let isEditting = "0"
+        fetchRequest.predicate  = NSPredicate(format: "isEditting == %@", isEditting)
+        fetchRequest.fetchLimit = 0
+        do {
+            let context = CoreDataStack.context
+            let count = try context.count(for: fetchRequest)
+            if count == 0 {
+                // no matching object
+                noDataView.isHidden = false
+                print("no present")
+            } else {
+                // at least one matching object exists
+                guard let edittingTravel = try? context.fetch(fetchRequest) else { return }
+                self.allTravel = edittingTravel
+                noDataView.isHidden = true
+            }
+        } catch let error as NSError {
+            print("Could not fetch \(error), \(error.userInfo)")
+        }
+        collectionView.reloadData()
+    }
 
 }
 
 extension NotificationViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        guard let count = allTravel?.count else {
+            return 0
+        }
+        return count
+    }
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        count = 0
+
+//        var allLocationPost = travel?.locationPosts?.allObjects as? [LocationPost]
+        var allLocationPost = allTravel?[section].locationPosts?.allObjects as? [LocationPost]
+        guard let allLocationCount = allLocationPost?.count else {
+            return 0
+        }
+        guard allLocationCount > 0 else {
+            return 0
+        }
+        for localPost in 0...allLocationCount - 1 {
+            guard let allLocationPhotoCount = allLocationPost?[localPost].photo?.count else {
+                return 0
+            }
+            count += allLocationPhotoCount
+        }
+        return count
+    }
+    func collectionView(_ collectionView: UICollectionView,
+                        viewForSupplementaryElementOfKind kind: String,
+                        at indexPath: IndexPath) -> UICollectionReusableView {
+        var reusableview: UICollectionReusableView!
+
+        //分区头
+        if kind == UICollectionView.elementKindSectionHeader {
+            reusableview = collectionView.dequeueReusableSupplementaryView(
+                ofKind: kind, withReuseIdentifier: "PhotoHeaderView", for: indexPath)
+            //设置头部标题
+            guard let label = reusableview.viewWithTag(1) as? UILabel else {
+                return reusableview
+            }
+            label.text = allTravel?[indexPath.section].title
+        }
+            //分区尾
+        else {
+            return reusableview
+        }
+        return reusableview
     }
     func collectionView(
         _ collectionView: UICollectionView,
@@ -54,6 +118,24 @@ extension NotificationViewController: UICollectionViewDelegate, UICollectionView
             withReuseIdentifier: String(describing: NormalPictureCollectionViewCell.self),
             for: indexPath
         )
-        return cell
+            guard let photoCell = cell as? NormalPictureCollectionViewCell else { return cell }
+            photos.removeAll()
+            var allLocationPost = allTravel?[indexPath.section].locationPosts?.allObjects as? [LocationPost]
+            for localPost in 0...allLocationPost!.count - 1 {
+                for index in 0...(allLocationPost?[localPost].photo!.count)! - 1 {
+                    photos.append((allLocationPost?[localPost].photo?[index])!)
+                }
+            }
+            let photo = photos[indexPath.row]
+            let nsDocumentDirectory = FileManager.SearchPathDirectory.documentDirectory
+            let nsUserDomainMask    = FileManager.SearchPathDomainMask.userDomainMask
+            let paths               = NSSearchPathForDirectoriesInDomains(nsDocumentDirectory, nsUserDomainMask, true)
+            if let dirPath = paths.first {
+                let imageURL = URL(fileURLWithPath: dirPath).appendingPathComponent(photo)
+                let image    = UIImage(contentsOfFile: imageURL.path)
+                photoCell.photoImageView.image = image
+                // Do whatever you want with the image
+            }
+        return photoCell
     }
 }
